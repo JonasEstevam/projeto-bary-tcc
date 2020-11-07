5ef9e7801a0fc84d909624b35ac1ae3d035bb23a
unknown <jonas_estevam@live.com>
2020-11-07 14:52:20 -0300

    inital commit

---
+++ src/app.js
@@ -0,0 +1,110 @@
+require('dotenv').config();
+
+const AdminBro = require('admin-bro');
+const AdminBroExpress = require('@admin-bro/express');
+const AdminBroMongoose = require('@admin-bro/mongoose');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+AdminBro.registerAdapter(AdminBroMongoose);
+
+const app = express();
+const port = 3000;
+
+const run = async () => {
+	const mongooseDB = await mongoose.connect(process.env.MONGODB_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+
+	const db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function () {
+		console.log('BD is Connected!');
+	});
+
+	const TagSchema = new mongoose.Schema({
+		tag: {
+			required: true,
+			type: String,
+		},
+		created_at: {
+			type: Date,
+			default: Date.now(),
+		},
+	});
+
+	const Tag = mongoose.model('Tag', TagSchema);
+
+	app.use(bodyParser.json());
+
+	const adminBro = new AdminBro({
+		dashboard: {
+			component: AdminBro.bundle('../dashboard'),
+		},
+		branding: {
+			companyName: 'TCC da Bary <3',
+			logo: 'https://i.ibb.co/ZJhd6Vr/logo.png',
+		},
+		locale: {
+			translations: {
+				labels: {
+					Tag: 'Todas as tags',
+				},
+				actions: {
+					new: 'Criar nova tag',
+					list: 'Todas as tags',
+					search: 'Procurar',
+					edit: 'Editar',
+					delete: 'Deletar',
+					show: 'Mostrar',
+					bulkDelete: 'Deletar todos os selecionados',
+				},
+			},
+		},
+		resources: [
+			{
+				resource: Tag,
+				options: {
+					actions: {
+						new: {
+							isVisible: false,
+						},
+					},
+					navigation: {name: 'Tags', icon: 'Tag'},
+					properties: {
+						_id: {
+							isVisible: {list: false, filter: false, show: false, edit: false},
+						},
+						created_at: {
+							isVisible: {list: true, filter: true, show: true, edit: false},
+						},
+					},
+				},
+			},
+		],
+		rootPath: '/admin',
+	});
+	const router = AdminBroExpress.buildRouter(adminBro);
+
+	app.use(adminBro.options.rootPath, router);
+
+	app.get('/', (req, res) => {
+		res.json({message: 'Hello World!'});
+	});
+
+	app.post('/tag', async (req, res) => {
+		const tag = req.body.tag;
+
+		const newTag = new TagModel({tag});
+		await newTag.save();
+		return res.send('Ok!');
+	});
+
+	app.listen(port, () => {
+		console.log(`Example app listening at http://localhost:${port}`);
+	});
+};
+
+run();
